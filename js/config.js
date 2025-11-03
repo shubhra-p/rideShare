0d8cb8b35da2a0c1d1896bba97e5b435247f3274
Shubhra Prakash <143318095+shubhra-p@users.noreply.github.com>
2025-11-03 20:30:13 +0530

    Update Cognito user pool configuration

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_lnkb2IJYY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2s3n0lijaa5p0qc0mgumcvfm58', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_33gLBKbf8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '674o1qlqfemgjo3c74bp3l6mlp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://gpr8rqffu4.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
